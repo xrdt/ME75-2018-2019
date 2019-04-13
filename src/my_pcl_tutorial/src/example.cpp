0ffdc374eafc95ab621bf5361f96802ac2710183
Bianca Yang <ipacifics@gmail.com>
2019-04-12 17:06:48 -0700

    new things

--- src/my_pcl_tutorial/src/example.cpp
+++ src/my_pcl_tutorial/src/example.cpp
@@ -19,8 +19,14 @@
 ros::Publisher pub1;
 ros::Publisher pub2;
 
+typedef pcl::PointXYZ PCLPoint;
+typedef pcl::PointCloud<pcl::PointXYZ> PCLPointCloud;
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_p (new pcl::PointCloud<pcl::PointXYZ>);
 sensor_msgs::PointCloud2::Ptr final_cloud (new sensor_msgs::PointCloud2);
+
+double m_groundFilterPlaneDistance = .5;
+
+PCLPointCloud ground, nonground;
 
 void 
 cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
@@ -32,7 +38,7 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 
   pcl::fromROSMsg (*input, *cloud_p);
 
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  // pcl::ExtractIndices<pcl::PointXYZ> extract;
 
   pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
   pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
@@ -41,10 +47,90 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   // Optional
   seg.setOptimizeCoefficients (true);
   // Mandatory
-  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setModelType (pcl::SACMODEL_PERPENDICULAR_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold(.5);
+  seg.setAxis(Eigen::Vector3f(0,0,1));
   seg.setDistanceThreshold (0.01);
+ 
+  // TODO
+  PCLPointCloud& pc = cloud;
+  //
+  PCLPointCloud cloud_filtered(pc);
 
+  pcl::ExtractIndices<PCLPoint> extract;
+  bool groundPlaneFound = false;
+
+
+  while (cloud_filtered.size() > 10 && !groundPlaneFound) {
+      seg.setInputCloud(cloud_filtered.makeShared());
+      seg.segment (*inliers, *coefficients);
+      if (inliers->indices.size () == 0){
+        ROS_INFO("PCL segmentation did not find any plane.");
+        break;
+      }
+
+      extract.setInputCloud(cloud_filtered.makeShared());
+      extract.setIndices(inliers);
+
+      if (std::abs(coefficients->values.at(3)) < m_groundFilterPlaneDistance){
+        ROS_DEBUG("Ground plane found: %zu/%zu inliers. Coeff: %f %f %f %f", inliers->indices.size(), cloud_filtered.size(),
+                  coefficients->values.at(0), coefficients->values.at(1), coefficients->values.at(2), coefficients->values.at(3));
+        extract.setNegative (false);
+        extract.filter (ground);
+
+        // remove ground points from full pointcloud:
+        // workaround for PCL bug:
+        if(inliers->indices.size() != cloud_filtered.size()){
+          extract.setNegative(true);
+          PCLPointCloud cloud_out;
+          extract.filter(cloud_out);
+          nonground += cloud_out;
+          cloud_filtered = cloud_out;
+        }
+
+        groundPlaneFound = true;
+      } else{
+        ROS_DEBUG("Horizontal plane (not ground) found: %zu/%zu inliers. Coeff: %f %f %f %f", inliers->indices.size(), cloud_filtered.size(),
+                  coefficients->values.at(0), coefficients->values.at(1), coefficients->values.at(2), coefficients->values.at(3));
+        pcl::PointCloud<PCLPoint> cloud_out;
+        extract.setNegative (false);
+        extract.filter(cloud_out);
+
+        // remove current plane from scan for next iteration:
+        // workaround for PCL bug:
+        if(inliers->indices.size() != cloud_filtered.size()){
+          extract.setNegative(true);
+          cloud_out.points.clear();
+          extract.filter(cloud_out);
+          cloud_filtered = cloud_out;
+        } else{
+          cloud_filtered.points.clear();
+        }
+      }
+
+    }
+    // TODO: also do this if overall starting pointcloud too small?
+    if (!groundPlaneFound){ // no plane found or remaining points too small
+      ROS_WARN("No ground plane found in scan");
+
+      // do a rough fitlering on height to prevent spurious obstacles
+      pcl::PassThrough<PCLPoint> second_pass;
+      second_pass.setFilterFieldName("z");
+      second_pass.setFilterLimits(-m_groundFilterPlaneDistance, m_groundFilterPlaneDistance);
+      second_pass.setInputCloud(pc.makeShared());
+      second_pass.filter(ground);
+
+      second_pass.setFilterLimitsNegative (true);
+      second_pass.filter(nonground);
+    }
+
+  sensor_msgs::PointCloud2 output;
+  pcl::toROSMsg(cloud_filtered, output);
+  pub1.publish(cloud_filtered);
+
+
+  /*
   seg.setInputCloud (cloud.makeShared ());
   seg.segment (*inliers, *coefficients);
 
@@ -62,6 +148,8 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   pcl::toROSMsg (*cloud_p, *final_cloud);
 
   pub1.publish (final_cloud); // publish the new pcl
+
+  */
 
   // Publish the model coefficients
   /*pcl_msgs::ModelCoefficients ros_coefficients;
