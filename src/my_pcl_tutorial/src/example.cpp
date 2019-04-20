e3989397f1efff1df37b825c88bd8eed6a65bbc0
Bianca Yang <ipacifics@gmail.com>
2019-04-19 17:15:10 -0700

    adding some comments for octomap pcl code

--- src/my_pcl_tutorial/src/example.cpp
+++ src/my_pcl_tutorial/src/example.cpp
@@ -8,6 +8,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/transforms.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
@@ -16,79 +17,93 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-ros::Publisher pub1;
-ros::Publisher pub2;
+// Initialize the plane and coefficient publishers
+ros::Publisher pub_plane;
+ros::Publisher pub_coefficient;
 
+// Taken out of Octomap 
 typedef pcl::PointXYZ PCLPoint;
 typedef pcl::PointCloud<pcl::PointXYZ> PCLPointCloud;
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_p (new pcl::PointCloud<pcl::PointXYZ>);
 sensor_msgs::PointCloud2::Ptr final_cloud (new sensor_msgs::PointCloud2);
 
+// All planes w / d < groundFilterPlaneDistance are eligible ground plane candidates
+// Ax + By + Cz + d = 0
 double m_groundFilterPlaneDistance = .5;
 
 PCLPointCloud ground, nonground;
 
+// Extract planes and their coefficients using the pcl library
 void 
 cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
 
   // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::PointCloud<PCLPoint> cloud;
   pcl::fromROSMsg (*input, cloud);
 
   pcl::fromROSMsg (*input, *cloud_p);
 
-  // pcl::ExtractIndices<pcl::PointXYZ> extract;
+  // Used later to extract all the inliers for a given plane.
+  pcl::ExtractIndices<PCLPoint> extract;
 
+  // Used to store plane coefficients.
   pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
   pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+
   // Create the segmentation object
   pcl::SACSegmentation<pcl::PointXYZ> seg;
+
   // Optional
   seg.setOptimizeCoefficients (true);
+
   // Mandatory
   seg.setModelType (pcl::SACMODEL_PERPENDICULAR_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold(.5);
-  seg.setAxis(Eigen::Vector3f(0,0,1));
+  seg.setDistanceThreshold(m_groundFilterPlaneDistance);
+  // Axis along wihch to search for a model perpendicular to
+  seg.setAxis(Eigen::Vector3f(0,1,0));
+  // Distance to model threshold
   seg.setDistanceThreshold (0.01);
  
-  // TODO
+  // Octomap stuff 
   PCLPointCloud& pc = cloud;
-  //
   PCLPointCloud cloud_filtered(pc);
 
-  pcl::ExtractIndices<PCLPoint> extract;
   bool groundPlaneFound = false;
-
 
   while (cloud_filtered.size() > 10 && !groundPlaneFound) {
       seg.setInputCloud(cloud_filtered.makeShared());
+      
       seg.segment (*inliers, *coefficients);
       if (inliers->indices.size () == 0){
         ROS_INFO("PCL segmentation did not find any plane.");
         break;
       }
 
+      // makeShared makes a deep copy of the cloud
       extract.setInputCloud(cloud_filtered.makeShared());
       extract.setIndices(inliers);
 
       if (std::abs(coefficients->values.at(3)) < m_groundFilterPlaneDistance){
         ROS_DEBUG("Ground plane found: %zu/%zu inliers. Coeff: %f %f %f %f", inliers->indices.size(), cloud_filtered.size(),
                   coefficients->values.at(0), coefficients->values.at(1), coefficients->values.at(2), coefficients->values.at(3));
+        // Negative=false means that all points will be returned
         extract.setNegative (false);
+        // Move all the indices into the ground PCLPointCloud 
         extract.filter (ground);
 
         // remove ground points from full pointcloud:
         // workaround for PCL bug:
         if(inliers->indices.size() != cloud_filtered.size()){
+          // Negative=true means that all the points except the input indices will be returned.
           extract.setNegative(true);
           PCLPointCloud cloud_out;
           extract.filter(cloud_out);
           nonground += cloud_out;
+          // All the non-ground stuff 
           cloud_filtered = cloud_out;
         }
-
         groundPlaneFound = true;
       } else{
         ROS_DEBUG("Horizontal plane (not ground) found: %zu/%zu inliers. Coeff: %f %f %f %f", inliers->indices.size(), cloud_filtered.size(),
@@ -102,6 +117,7 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
         if(inliers->indices.size() != cloud_filtered.size()){
           extract.setNegative(true);
           cloud_out.points.clear();
+          // Move all the negative points into 
           extract.filter(cloud_out);
           cloud_filtered = cloud_out;
         } else{
@@ -129,8 +145,6 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   pcl::toROSMsg(cloud_filtered, output);
   pub1.publish(cloud_filtered);
 
-
-  /*
   seg.setInputCloud (cloud.makeShared ());
   seg.segment (*inliers, *coefficients);
 
@@ -149,15 +163,12 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 
   pub1.publish (final_cloud); // publish the new pcl
 
-  */
 
   // Publish the model coefficients
-  /*pcl_msgs::ModelCoefficients ros_coefficients;
+  pcl_msgs::ModelCoefficients ros_coefficients;
   pcl_conversions::fromPCL(coefficients, ros_coefficients);
   pub2.publish (ros_coefficients);
-  */
 
-  /*
   // Container for original & filtered data
   pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
   pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
@@ -178,7 +189,6 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 
   // Publish the data
   pub.publish (output);
-  */
 }
 
 int
@@ -192,8 +202,8 @@ main (int argc, char** argv)
   ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
-  pub1 = nh.advertise<sensor_msgs::PointCloud2> ("output2", 1);
-  pub2 = nh.advertise<pcl_msgs::ModelCoefficients> ("testing", 1);
+  pub_plane = nh.advertise<sensor_msgs::PointCloud2> ("output2", 1);
+  pub_coefficient = nh.advertise<pcl_msgs::ModelCoefficients> ("testing", 1);
 
   // Spin
   ros::spin ();
